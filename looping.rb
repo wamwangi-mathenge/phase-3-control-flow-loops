821d05b96b11768b349b883c9fc9013cf8bd92b9
Brian <wamwangimathenge@gmail.com>
2023-02-07 18:54:54 +0300

    Completed assignment

--- looping.rb
+++ looping.rb
@@ -23,6 +23,17 @@ end
 
 def fizzbuzz_printer
   # your code here
+  1.upto(100) do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
 
 def reverse_string(str)
